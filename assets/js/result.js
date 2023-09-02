99782630e54b48da1af61b4f8bc6d1b8627dd594
JoaquimSP <joaquimsp2000@gmail.com>
2023-09-02 12:35:47 -0300

    :sparkles: feat: calculating the value

--- assets/js/result.js
+++ assets/js/result.js
@@ -3,6 +3,14 @@ window.addEventListener('load', () => {
     const cost = localStorage.getItem('Custo');
     /* const cost = parseFloat(localStorage.getItem('Valor')); */
 
+    const cost1 = cost * 3;
+    const cost2 = cost * 6;
+    const cost3 = cost * 12;
+
     document.getElementById('result-name').innerHTML = name;
+    
     document.getElementById('result-value').innerHTML = cost;
+    document.getElementById('result-value1').innerHTML = cost1;
+    document.getElementById('result-value2').innerHTML = cost2;
+    document.getElementById('result-value3').innerHTML = cost3;
 })
