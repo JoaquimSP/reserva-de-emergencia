820a40eb8c341dd572f14ddd1c4398db53deba9f
JoaquimSP <joaquimsp2000@gmail.com>
2023-09-05 19:19:21 -0300

    :ok_hand: new settings

--- assets/js/result.js
+++ assets/js/result.js
@@ -7,10 +7,14 @@ window.addEventListener('load', () => {
     const cost2 = cost * 6;
     const cost3 = cost * 12;
 
+    const value1 = cost1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const value2 = cost2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const value3 = cost3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
     document.getElementById('result-name').innerHTML = name;
 
     /* document.getElementById('result-value').innerHTML = cost; */
-    document.getElementById('result-value1').innerHTML = cost1;
-    document.getElementById('result-value2').innerHTML = cost2;
-    document.getElementById('result-value3').innerHTML = cost3;
+    document.getElementById('result-value1').innerHTML = value1;
+    document.getElementById('result-value2').innerHTML = value2;
+    document.getElementById('result-value3').innerHTML = value3;
 })
